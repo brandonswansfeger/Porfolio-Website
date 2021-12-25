4eb1a69e5fcc3a462d90c804e319b697cfd90803
Brandon Swansfeger <88030396+brandonswansfeger@users.noreply.github.com>
2021-12-25 03:34:02 -0500

    Update Skills.js

--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -1,10 +1,29 @@
 import React from "react";
-import Onestock from "../Components/Onestock";
+
 
 const Skills = (props) => {
   return (
-  <div className="Onestock">
-    <Onestock />
+  <div className="SkillsBody">
+        <p style={{fontFamily:'Federo', fontSize:'50px', color:'turquoise', marginTop:'5%', marginBottom:'3%', textAlign:'center'}}>Technical Skills</p>
+<div className="SkillsIcons">
+  <i class="devicon-javascript-plain" style={{color:"turquoise", fontSize:'70px'}}></i>
+  <i class="devicon-html5-plain-wordmark"style={{color:'white', fontSize:'70px'}}></i>
+  <i class="devicon-css3-plain-wordmark" style={{color:"turquoise", fontSize:'70px'}}></i>
+  <i class="devicon-react-original-wordmark" style={{color:'white', fontSize:'70px'}}></i>
+  <i class="devicon-nodejs-plain-wordmark" style={{color:"turquoise", fontSize:'70px'}}></i>
+  <i class="devicon-express-original-wordmark" style={{color:'white', fontSize:'70px'}}></i>
+  <i class="devicon-postgresql-plain-wordmark" style={{color:"turquoise", fontSize:'70px'}}></i>
+  <i class="devicon-mongodb-plain-wordmark" style={{color:'white', fontSize:'70px'}}></i>
+  <i class="devicon-pandas-original-wordmark" style={{color:"turquoise", fontSize:'70px'}}></i>
+  <i class="devicon-python-plain-wordmark" style={{color:'white', fontSize:'70px'}}></i>
+  <i class="devicon-django-plain-wordmark" style={{color:"turquoise", fontSize:'70px'}}></i>
+  <i class="devicon-ubuntu-plain-wordmark" style={{color:'white', fontSize:'70px'}}></i>
+  <i class="devicon-bootstrap-plain-wordmark" style={{color:"turquoise", fontSize:'70px'}}></i>
+  <i class="devicon-github-original-wordmark" style={{color:'white', fontSize:'70px'}}></i>
+  <i class="devicon-heroku-line-wordmark" style={{color:"turquoise", fontSize:'70px'}}></i>
+  <i class="devicon-jquery-plain-wordmark" style={{color:'white', fontSize:'70px'}}></i>
+ 
+   </div>
   </div>
   ) 
 };
