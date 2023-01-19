e76313775dfe0dd009d8556ae3bcc42d6f416073
Brandon Swansfeger <brandonswansfeger@gmail.com>
2023-01-18 21:46:39 -0500

    remove

--- src/App.js
+++ src/App.js
@@ -9,10 +9,18 @@ import Skills from "./pages/Skills";
 import Contact from "./pages/Contact";
 import { Routes } from "react-router";
 import Nav from "./Components/Nav";
+// import { useState, useEffect } from 'react';
+// import LoadingScreen from './loading'
 
 
 function App() {
+  // const [lyricsItem, etLyricsItem] = useState(null);
+  // const [loading, setLoading] = useState(false);
+  // useEffect(() => {
+  //   setTimeout(() => setLoading(false), 6000)
+  // }, [])
 return (
+  // {loading ? (
 <div className="App">
   <Nav />
   <Routes>
@@ -23,7 +31,9 @@ return (
     <Route path="/Contact" element={<Contact/>} />
  </Routes>
    </div>
+  
   );
 }
+
 export default App;
 
