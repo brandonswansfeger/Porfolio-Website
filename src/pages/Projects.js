24f8cc397a3ec5c0c4e920e45e9151b8c4030497
Brandon Swansfeger <brandonswansfeger@gmail.com>
2023-01-19 10:49:56 -0500

    layout

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -15,7 +15,7 @@ export default function Projects() {
                 <img class="GDPImage" src={landingPage} alt="Project0" height="150px" width="210px" style={{marginLeft:'30px', marginRight:'30px', marginTop:'10px',marginBottom:'5px', boxShadow:'4px 6px 15px 9px rgba(0,0,0,0.76)', justifyContent:'flexStart'}}/>
                 <p style={{fontSize:'22.5px', lineHeight:'22px', color:'black', width:'270px', textAlign:'center', justifySelf:'top', fontWeight:'bold', justifyContent:'flexStart', marginTop:'20px', marginBottom:'-5px'}}>World Bank Data Visualization: GDP & TFR</p>
                 <p style={{fontSize:'18px', color:'black', width:'260px', textAlign:'center', justifySelf:'top', justifyContent:'flexStart', fontWeight:'light', marginTop:'20px', marginBottom:'10px'}}>Fetch external data based on user input and display/render bar chart showing GDP & TFR for all countries</p>
-                <a class="button" href="https://tfr-gdp-bar-chart.herokuapp.com/" data-maxbuttoncounter="{&quot;button_id&quot;:204038,&quot;url&quot;:&quot;[empty]&quot;}" data-maxbuttonpage="{&quot;id&quot;:false,&quot;type&quot;:false}"><span class='mb-text' >Demo</span></a>
+                <a class="button disabled" href="https://tfr-gdp-bar-chart.herokuapp.com/" data-maxbuttoncounter="{&quot;button_id&quot;:204038,&quot;url&quot;:&quot;[empty]&quot;}" data-maxbuttonpage="{&quot;id&quot;:false,&quot;type&quot;:false}"><span class='mb-text' >Demo</span></a>
                 <a class="button" href="https://github.com/brandonswansfeger/GDP-Bar-Charts" data-maxbuttoncounter="{&quot;button_id&quot;:204038,&quot;url&quot;:&quot;[empty]&quot;}" data-maxbuttonpage="{&quot;id&quot;:false,&quot;type&quot;:false}"><span class='mb-text' >Github</span></a>
 
             
@@ -56,7 +56,7 @@ export default function Projects() {
         </div>
 <img src={Project3} alt="Project1" width="95%" height="65%" style={{marginLeft:'5px', marginRight:'5px', marginTop:'5px'}}/>
 <div class="ButtonLink1">
-<a class="button" disabled href="https://dc-dev-to-dev-test.herokuapp.com/" data-maxbuttoncounter="{&quot;button_id&quot;:204038,&quot;url&quot;:&quot;[empty]&quot;}" data-maxbuttonpage="{&quot;id&quot;:false,&quot;type&quot;:false}" style={{marginTop:'20px'}}><span class='mb-text' >Demo</span></a>
+<a class="button disabled" href="https://dc-dev-to-dev-test.herokuapp.com/" data-maxbuttoncounter="{&quot;button_id&quot;:204038,&quot;url&quot;:&quot;[empty]&quot;}" data-maxbuttonpage="{&quot;id&quot;:false,&quot;type&quot;:false}" style={{marginTop:'20px'}}><span class='mb-text' >Demo</span></a>
 </div>
 <div class="ButtonLink2">
 <a class="button" href="https://github.com/brandonswansfeger/dev-to-dev" data-maxbuttoncounter="{&quot;button_id&quot;:204038,&quot;url&quot;:&quot;[empty]&quot;}" data-maxbuttonpage="{&quot;id&quot;:false,&quot;type&quot;:false}"><span class='mb-text' >Github</span></a>
