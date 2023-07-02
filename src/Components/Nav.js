d257550c93e7c6e8c44dc3855c250393ac3ef8e5
Brandon Swansfeger <88030396+brandonswansfeger@users.noreply.github.com>
2023-07-01 21:13:01 -0400

    update videos

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { NavLink } from 'react-router-dom'
 // import { Link } from "react-router-dom";
 
 const Nav = (props) => {
@@ -6,24 +7,24 @@ const Nav = (props) => {
     <div className="NavWrapper" style={{position:'fixed', top: '0px', right: '0px'}}>
 
  {/* <div className="NavBar" style={{height:'40px', justifyContent:'flex-end', flexWrap:'nowrap', alignSelf: 'flex-start'}}> */}
-    <a href="/" className="ButtonBorder navButton">
+    <NavLink to="/" className="ButtonBorder navButton" activeClassName="is-active">
     <span style={{ backgroundColor:'transparent', color:'turquoise'}}>Home</span>
-    </a> 
-    <a href="/Aboutme/" className="ButtonBorder navButton">
+    </NavLink> 
+    <NavLink to="/Aboutme/" className="ButtonBorder navButton" activeClassName="is-active">
       <span style={{ backgroundColor:'transparent', color:'turquoise'}}>About</span>
-    </a>
-    <a href="/Workcopy/"className="ButtonBorder navButton">
+    </NavLink>
+    <NavLink to="/Workcopy/"className="ButtonBorder navButton" activeClassName="is-active">
     <span style={{ backgroundColor:'transparent', color:'turquoise'}}>Work</span>
-    </a>
-    <a href="/Projects/" className="ButtonBorder navButton">
+    </NavLink>
+    <NavLink to="/Projects/" className="ButtonBorder navButton" activeClassName="is-active">
     <span style={{ backgroundColor:'transparent', color:'turquoise'}}>Projects</span>
-    </a>
-    <a href="/Skills/"className="ButtonBorder navButton">
+    </NavLink>
+    <NavLink to="/Skills/"className="ButtonBorder navButton" activeClassName="is-active">
     <span style={{ backgroundColor:'transparent', color:'turquoise'}}>Skills</span>
-    </a>
-    <a href="/Contact/" className="ButtonBorder navButton">
+    </NavLink>
+    <NavLink to="/Contact/" className="ButtonBorder navButton" activeClassName="is-active">
     <span  style={{ backgroundColor:'transparent', color:'turquoise'}}>Contact</span>
-    </a>
+    </NavLink>
 
     </div>
    
